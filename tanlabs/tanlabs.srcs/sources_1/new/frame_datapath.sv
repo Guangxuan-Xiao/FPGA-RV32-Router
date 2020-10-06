7667fa2fd0cb8976136f3953c0621182844eecfc
lvt18 <lvt_thu18@126.com>
2020-10-06 19:34:23 +0800

    add ARP parser in loopback.

--- tanlabs/tanlabs.srcs/sources_1/new/frame_datapath.sv
+++ tanlabs/tanlabs.srcs/sources_1/new/frame_datapath.sv
@@ -58,8 +58,10 @@ module frame_datapath
         .m_axis_tuser(in.user)
     );
 
-    assign in.drop = 1'b0;
-    assign in.drop_next = 1'b0;
+    assign in.drop       = 1'b0;
+    assign in.drop_next  = 1'b0;
+    assign in.dont_touch = 1'b0;
+    assign in.dest       = 0;
 
     // Track frames and figure out when it is the first beat.
     always @ (posedge eth_clk or posedge reset)
@@ -80,14 +82,120 @@ module frame_datapath
     // README: Your code here.
     // See the guide to figure out what you need to do with frames.
 
-    frame_data out;
-    always @ (*)
+    frame_data s1;
+    wire s1_ready;
+    assign in_ready = s1_ready || !in.valid;
+    always @ (posedge eth_clk or posedge reset)
     begin
-        out = in;
-        out.data[`MAC_DST] <= in.data[`MAC_SRC];
-        out.data[`MAC_SRC] <= in.data[`MAC_DST];
-        out.dest = in.id;  // Loopback
-    end
+        if (reset)
+        begin
+            s1 <= 0;
+        end
+        else if (s1_ready)
+        begin
+            s1 <= in;
+            if (in.valid && in.is_first && !in.drop && !in.dont_touch)
+            begin
+                s1.data[`MAC_DST] <= in.data[`MAC_SRC];
+                s1.data[`MAC_SRC] <= in.data[`MAC_DST];
+            end
+        end
+            end
+            
+            frame_data s2;
+            wire s2_ready;
+            assign s1_ready = s2_ready || !s1.valid;
+            always @ (posedge eth_clk or posedge reset)
+            begin
+                if (reset)
+                begin
+                    s2 <= 0;
+                end
+                else if (s2_ready)
+                begin
+                    s2 <= s1;
+                    if (s1.valid && s1.is_first && !s1.drop && !s1.dont_touch)
+                    begin
+                        if ( s1.data[`MAC_TYPE] != ETHERTYPE_ARP )
+                        begin
+                            s2.drop <= 1'b1;
+                        end
+                    end
+                end
+                    end
+
+            frame_data s3;
+            wire s3_ready;
+            assign s2_ready = s3_ready || !s2.valid;
+            always @ (posedge eth_clk or posedge reset)
+            begin
+                if(reset)
+                begin
+                    s3 <= 0;
+                end
+                else if (s3_ready)
+                begin
+                    s3 <= s2;
+                    if (s2.valid && s2.is_first && !s2.drop && !s2.dont_touch)
+                    begin
+                        op <= s3.data[`OP];
+                    end
+                end
+            end
+                    
+                    
+                    
+                    frame_data s4;
+                    wire s4_ready;
+                    assign s3_ready = s4_ready || !s3.valid;
+                    always @ (posedge eth_clk or posedge reset)
+                    begin
+                        if (reset)
+                        begin
+                            s4 <= 0;
+                        end
+                        else if (s4_ready)
+                        begin
+                            s4 <= s3;
+                            if (s3.valid && s3.is_first && !s3.drop && !s3.dont_touch)
+                            begin
+                                if(op == REQUEST)
+                                begin
+                                    s4.data[`SRC_IP_ADDR] <= s3.data[`TRG_IP_ADDR];
+                                    s4.data[`SRC_MAC_ADDR] <= LOCAL_MAC;
+                                    s4.data[`TRG_IP_ADDR] <= s3.data[`SRC_IP_ADDR];
+                                    s4.data[`TRG_MAC_ADDR] <= s3.data[`SRC_MAC_ADDR];
+                                end
+                                else if(op == REPLY)
+                                begin
+                                    src_ip_addr <= s3.data[`SRC_IP_ADDR];
+                                    src_mac_addr <= s3.data[`SRC_MAC_ADDR];
+                                end
+                            end
+                        end
+                    end
+                            
+                            frame_data s5;
+                            wire s5_ready;
+                            assign s4_ready = s5_ready || !s4.valid;
+                            always @ (posedge eth_clk or posedge reset)
+                            begin
+                                if (reset)
+                                begin
+                                    s5 <= 0;
+                                end
+                                else if (s5_ready)
+                                begin
+                                    s5 <= s4;
+                                    if (s4.valid && s4.is_first && !s4.drop && !s4.dont_touch)
+                                    begin
+                                        s5.dest <= s4.id;
+                                    end
+                                end
+                                    end
+                                    
+    frame_data out;
+    assign out = s5;
 
     wire out_ready;
     assign in_ready = out_ready || !out.valid;
