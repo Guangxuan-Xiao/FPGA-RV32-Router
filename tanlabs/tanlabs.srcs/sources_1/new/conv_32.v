4b7500ef56ec3e75f821f58b90ac5c9fda3b520d
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-10-12 21:52:08 +0800

    conv syntax error fixed

--- tanlabs/tanlabs.srcs/sources_1/new/conv_32.v
+++ tanlabs/tanlabs.srcs/sources_1/new/conv_32.v
@@ -1,10 +1,7 @@
-module conv_32
-(
-    input wire [31:0] wire_in,
-    output wire [31:0] wire_out
-)
-
+module conv_32 (input wire [31:0] wire_in,
+                output wire [31:0] wire_out);
 assign wire_out[31:24] = wire_in[7:0];
 assign wire_out[23:16] = wire_in[15:8];
-assign wire_out[15:8] = wire_in[23:16];
-assign wire_out[7:0] = wire_in[31:24];
+assign wire_out[15:8]  = wire_in[23:16];
+assign wire_out[7:0]   = wire_in[31:24];
+endmodule
