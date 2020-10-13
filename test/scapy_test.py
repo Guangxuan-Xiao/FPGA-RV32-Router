49daf54e1d34d3fae5cc70357c8745bf0d514115
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-10-13 22:39:41 +0800

    new scapy script

--- test/scapy_test.py
+++ test/scapy_test.py
@@ -1,13 +1,4 @@
 from scapy.all import *
-a = IP(ttl = 10)
-print(a)
-print(a.src)
-a.dst="192.168.1.1"
-print(a.src)
-del(a.ttl)
-print(a.ttl)
-send(IP(dst="1.2.3.4"))
-#enx00e04c680024
-#enx00e04c680024
-#sendp("I'm travelling on Ethernet", iface="enx00e04c680024", loop=1, inter=0.2)
-send(IP(dst='170.170.170.1'))
+dst = '187.187.187.0'
+while True:
+    send(IP(ttl=255, dst=dst), iface="enx000ec650a042")
