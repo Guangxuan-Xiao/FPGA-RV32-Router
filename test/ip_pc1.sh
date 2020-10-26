0f0961e7e4bca95c674a847633b9e48db84a22f9
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-10-26 14:52:43 +0800

    ip pc1 script

--- test/ip_pc1.sh
+++ test/ip_pc1.sh
@@ -1,8 +1,11 @@
+PORT0=enx00e04c683cb7
 PORT1=enx00e04c680024
-PC_IP_1=187.187.187.187
+BOARD_IP_0=170.170.170.0
 BOARD_IP_1=187.187.187.0
-# sudo ifconfig $PORT1 $PC_IP_1
+PC_IP_0=170.170.170.170
+PC_IP_1=187.187.187.187
 sudo ip addr add ${PC_IP_1}/8 dev $PORT1
 sudo ip route add 170.0.0.0/8 via ${BOARD_IP_1} dev $PORT1
 sudo arping -I $PORT1 $BOARD_IP_1 -c 2
 iperf3 -s
+# sudo ping -f $PC_IP_0
