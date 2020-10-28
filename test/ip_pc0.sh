0ce712bcbfd15bd2d47b12f9a807ed34156e5f0a
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-10-28 19:58:43 +0800

    runnable but has timing issue version

--- test/ip_pc0.sh
+++ test/ip_pc0.sh
@@ -7,5 +7,5 @@ PC_IP_1=187.187.187.187
 sudo ip addr add ${PC_IP_0}/8 dev $PORT0
 sudo ip route add 187.0.0.0/8 via ${BOARD_IP_0} dev $PORT0
 sudo arping -I ${PORT0} ${BOARD_IP_0} -c 2
-iperf3 -c $PC_IP_1 -t 10 -R -i 2
+# iperf3 -c $PC_IP_1 -t 10 -R -i 2
 # sudo ping -f $PC_IP_1
