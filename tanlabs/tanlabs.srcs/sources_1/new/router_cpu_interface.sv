e1c19be57ffc00a0896a3ab11c738344d4a128ee
吕天 <2018011391@secoder.net>
2020-12-08 14:22:19 +0800

    Add router control logic.

--- tanlabs/tanlabs.srcs/sources_1/new/router_cpu_interface.sv
+++ tanlabs/tanlabs.srcs/sources_1/new/router_cpu_interface.sv
@@ -26,8 +26,105 @@ module router_cpu_interface(
     output [31:0] cpu_data_o
 );
 
+typedef enum reg[1:0] { START, ACCESS, END } router_write_state;
 
+// Showing the state of router write.
+always @ (posedge clk_router)
+begin
+   if (rst)
+   begin
+     router_write_state <= START;
+   end
+   else
+   begin
+     if (!internal_rx_ready || !internal_rx_valid)
+     begin
+       router_write_state <= START;
+     end
+     else if (internal_rx_ready && internal_rx_valid && !internal_rx_last)
+     begin
+       router_write_state <= ACCESS;
+     end
+     else if (internal_rx_ready && internal_rx_valid && internal_rx_last)
+     begin
+       router_write_state <= END;
+     end
+     else
+     begin
+       router_write_state <= START;
+     end
+   end
+end
 
+reg cpu_done;
+reg router_is_last;
+reg [17:0] bram_b_addr;  //cpi -> router 
+reg [17:0] bram_b_pointer;
+reg [8:0]  bram_b_data;
+typedef enum reg[1:0] { START, ACCESS, END } router_read_state;
+//Showing the process of router read.
+always @ (posedge clk_router)
+begin
+  if (rst)
+  begin
+        internal_tx_ready <= 0;
+        internal_tx_valid <= 0;
+        internal_tx_last <= 0;
+        internal_tx_data <= 0;
+        bram_b_addr <= 0;
+        router_read_state <= START;
+  end 
+  else
+  begin
+    if (cpu_done)
+    begin
+      if (router_read_state == START)
+      begin
+        internal_tx_ready <= 1;
+        internal_tx_valid <= 1;
+        internal_tx_last <= 0;
+        internal_tx_data <= 0;
+        bram_b_addr <= bram_b_pointer;
+        router_read_state <= ACCESS;
+      end
+      else if (router_read_state == ACCESS)
+      begin
+        if (router_is_last)
+        begin
+          internal_tx_last <= 1;
+          internal_tx_data <= bram_b_data;
+          bram_b_addr <= bram_b_addr + 4 ; // Uncertain.
+          router_read_state <= END;
+        end
+        else 
+        begin
+          internal_tx_last <= 1;
+          internal_tx_data <= bram_b_data;
+          bram_b_addr <= bram_b_addr + 4 ; // Uncertain.
+          router_read_state <= ACCESS;
+        end
+      end
+      else
+      begin
+        internal_tx_last <= 0;
+        internal_tx_ready <= 0;
+        internal_tx_valid <= 0;
+        internal_tx_data  <= 0;
+        bram_b_addr <= 0;
+        router_read_state <= START;
+      end
+    end
+    else
+    begin
+        internal_tx_ready <= 0;
+        internal_tx_valid <= 0;
+        internal_tx_last <= 0;
+        internal_tx_data <= 0;
+        bram_b_addr <= 0;
+        router_read_state <= START;
+    end
+  end
+end
 
 // A for router, B for CPU
 blk_mem_gen_3 data_interface 
