5400a24ce2707ebdead8b8ef763969c958405ee0
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-12-08 10:47:21 +0800

    hardware forwarding table passed all software cases

--- firmware/main.c
+++ firmware/main.c
@@ -5,6 +5,44 @@
 #include <route_table.h>
 extern uint32_t _bss_begin[];
 extern uint32_t _bss_end[];
+void test()
+{
+    RoutingTableEntry entry1 = {
+        .ip = 0x00030201,
+        .prefix_len = 24,
+        .port = 9,
+        .nexthop_ip = 0x0203a8c0};
+    insert(entry1);
+    RoutingTableEntry entry2 = {
+        .ip = 0x04030201,
+        .prefix_len = 32,
+        .port = 10,
+        .nexthop_ip = 0x0109a8c0};
+    insert(entry2);
+    uint32_t nexthop_ip, port;
+    search(0x04030201, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    search(0x01030201, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    search(0x00000000, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    remove(0x04030201, 32);
+    search(0x04030201, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    search(0x01030201, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    search(0x00000000, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    remove(0x00030201, 24);
+    search(0x04030201, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    search(0x01030201, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    search(0x00000000, &nexthop_ip, &port);
+    printf("Nexthop: 0x%08x\nPort: %d\n", nexthop_ip, port);
+    // search(entry.ip, &nexthop_ip, &port);
+    // printf("Nexthop: %x\nPort: %x\n", nexthop_ip, port);
+}
 
 void start(void)
 {
@@ -16,18 +54,7 @@ void start(void)
     init_uart();
 
     printf("hello, world\n");
-    RoutingTableEntry entry = {
-        .ip = 0x12345678,
-        .prefix_len = 24,
-        .port = 3,
-        .nexthop_ip = 0xabcdabcd};
-    insert(entry);
-    uint32_t nexthop_ip, port;
-    search(entry.ip, &nexthop_ip, &port);
-    printf("Nexthop: %x\nPort: %x\n", nexthop_ip, port);
-    remove(entry.ip, entry.prefix_len);
-    search(entry.ip, &nexthop_ip, &port);
-    printf("Nexthop: %x\nPort: %x\n", nexthop_ip, port);
+    test();
     while (true)
         ;
 }
