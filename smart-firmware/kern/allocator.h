2a2142994e7ddddb22aa55d959a4cd538b0e901b
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-12-10 17:23:11 +0800

    allocator done

---
+++ smart-firmware/kern/allocator.h
@@ -0,0 +1,22 @@
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+#include <stdint.h>
+#include <stdlib.h>
+struct AllocatorNode
+{
+    int16_t succ;
+    uint16_t idx;
+};
+class Allocator
+{
+private:
+    AllocatorNode *nodes;
+    uint16_t allocated;
+public:
+    Allocator(uint16_t size);
+    ~Allocator();
+    uint16_t get();
+    void put(uint16_t idx);
+};
+
+#endif // !1
