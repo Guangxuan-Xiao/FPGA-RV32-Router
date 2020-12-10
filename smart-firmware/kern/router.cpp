514c710ed25968f7391fb0685d42a8067faf5c5a
zzz <zhang-zz18@mails.tsinghua.edu.cn>
2020-12-10 19:42:44 +0800

    small bug fixed

--- smart-firmware/kern/router.cpp
+++ smart-firmware/kern/router.cpp
@@ -6,10 +6,10 @@ static volatile uint32_t *const ip1_ptr = (volatile uint32_t *)0x10000110;
 static volatile uint32_t *const ip2_ptr = (volatile uint32_t *)0x10000120;
 static volatile uint32_t *const ip3_ptr = (volatile uint32_t *)0x10000130;
 static volatile uint32_t *const mac_prefix_ptr = (volatile uint32_t *)0x10000200;
-static volatile uint32_t *const buffer_read_start_addr = (volatile uint32_t *)0x60000000;
-static volatile uint32_t *const buffer_write_start_addr = (volatile uint32_t *)0x60040000;
-static volatile uint32_t *const buffer_size_addr = (volatile uint32_t *)0x7FC;
-static uint32_t const buffer_width = 10;
+static uint32_t const read_start = 0x60000000;
+static uint32_t const write_start = 0x60040000;
+static uint32_t const size_addr = 0x7FC;
+static uint32_t const width = 11;
 
 void set_ip(uint32_t ip0, uint32_t ip1, uint32_t ip2, uint32_t ip3)
 {
@@ -37,14 +37,14 @@ uint32_t read_mac_prefix()
     return *mac_prefix_ptr;
 }
 
-uint32_t receive(uint8_t *buffer, uint32_t src)
+uint32_t receive(uint8_t* buffer, uint32_t src)
 {
-    volatile uint32_t *ptr;
-    ptr = (volatile uint32_t *)(buffer_read_start_addr + (src << buffer_width) + buffer_size_addr);
+    volatile uint32_t* ptr;
+    ptr = (volatile uint32_t*)(read_start + (src << width) + size_addr);
     uint32_t buf = 0;
     uint32_t len = *ptr;
     len = ((len & 0xFF000000) >> 24) + ((len & 0xFF0000) >> 8);
-    ptr = (volatile uint32_t *)(buffer_read_start_addr + (src << buffer_width));
+    ptr = (volatile uint32_t*)(read_start + (src << width));
     for (uint32_t i = 0; i < len; i = i + 4)
     {
         buf = *ptr;
@@ -58,11 +58,11 @@ uint32_t receive(uint8_t *buffer, uint32_t src)
     return len;
 }
 
-void send(uint8_t *buffer, uint32_t len, uint32_t dst);
+void send(uint8_t* buffer, uint32_t len, uint32_t dst)
 {
     uint32_t i = 0;
-    volatile uint32_t *ptr;
-    ptr = (volatile uint32_t *)(buffer_write_start_addr + (dst << buffer_width));
+    volatile uint32_t* ptr;
+    ptr = (volatile uint32_t*)(write_start + (dst << width));
     uint32_t buf = 0;
     len = len - 4;
     for (i = 0; i < len; i = i + 4)
@@ -75,10 +75,10 @@ void send(uint8_t *buffer, uint32_t len, uint32_t dst);
     buf = 0;
     for (; i < len; i++)
     {
-        buf = buf + (buffer[i] << (i % 4));
+        buf = buf + (buffer[i] << (i & 3));
     }
     *ptr = buf;
-    ptr = (volatile uint32_t *)(buffer_read_start_addr + (dst << buffer_width) + buffer_size_addr);
+    ptr = (volatile uint32_t*)(read_start + (dst << width) + size_addr);
     buf = ((len & 0xFF) << 24) + ((len & 0xFF00) << 8);
     *ptr = buf;
 }
