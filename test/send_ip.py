fcf535a9124322acaf83a51b48f20f51228b2662
zzz <zhang-zz18@mails.tsinghua.edu.cn>
2020-12-12 11:19:43 +0800

    说白了还是条懒狗

--- test/send_ip.py
+++ test/send_ip.py
@@ -1,10 +1,14 @@
 from scapy.all import *
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--dst", type=str, required=True)
-parser.add_argument("--iface", type=str, required=True)
-args = parser.parse_args()
-dst = args.dst
-iface = args.iface
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--dst", type=str, required=True)
+# parser.add_argument("--iface", type=str, required=True)
+# args = parser.parse_args()
+# dst = args.dst
+# iface = args.iface
 while True:
-    send(IP(ttl=255, dst=dst), iface=iface)
+    # send(IP(ttl=255, dst=dst), iface=iface)
+    send(IP(ttl=255, dst="172.172.172.172"), iface="enx00e04c68002d")
+# sudo ip addr add 172.172.172.172/8 dev enx00e04c68002d
+# sudo minicom -b 9600 -D /dev/ttyACM0
+# sudo arping -I enx00e04c68002d 172.172.172.172
