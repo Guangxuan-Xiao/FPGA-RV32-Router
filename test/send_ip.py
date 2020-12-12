c7fe2bb654d2be5fec3534669c2db3ed618b630c
zzz <zhang-zz18@mails.tsinghua.edu.cn>
2020-12-12 14:32:09 +0800

    我的理解加深了。

--- test/send_ip.py
+++ test/send_ip.py
@@ -9,7 +9,7 @@ from scapy.all import *
 # while True:
     # send(IP(ttl=255, dst=dst), iface=iface)
 send(IP(ttl=255, dst="68.68.68.68"), iface="enx00e04c68002d")
-# sudo ip addr add 172.172.172.172/8 dev enx00e04c68002d
-# sudo arping -I enx00e04c68002d 172.172.172.172 -c 2
+# sudo ip addr add 68.68.68.69/8 dev enx00e04c68002d
+# sudo arping -I enx00e04c68002d 68.68.68.68 -c 2
 # sudo minicom -b 9600 -D /dev/ttyACM0
 # sudo arping -I enx00e04c68002d 172.172.172.172
