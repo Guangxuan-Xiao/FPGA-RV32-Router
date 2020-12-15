719c8c49e6df6467f83776795514fdef82c94bc9
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-12-15 22:49:01 +0800

    debugging.

--- smart-firmware/kern/timer.h
+++ smart-firmware/kern/timer.h
@@ -4,10 +4,24 @@
 #include <stdio.h>
 const uint32_t NS_PER_CLOCK = 20;
 const uint32_t CLOCK_PER_SEC = 50000000;
-const uint32_t CLOCK_PER_MS = 50000;
+const uint32_t CLOCK_PER_MS =  50000;
+const uint32_t CLOCK_PER_US =  50;
 uint32_t get_clock()
 {
     return *((volatile uint32_t *)0x10000010);
+}
+
+void sleep(uint32_t usec)
+{
+    uint32_t last_clock = get_clock(), current_clock = get_clock();
+    while (1)
+    {
+        current_clock = get_clock();
+        if (current_clock - last_clock >= usec * CLOCK_PER_US)
+        {
+            return;
+        }
+    }
 }
 
 void clock_test()
