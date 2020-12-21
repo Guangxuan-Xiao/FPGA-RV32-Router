7c22ae568a7fb1824776a1066d0fa74a7e366288
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-12-22 00:29:37 +0800

    hardware endian

--- test/send_rip.py
+++ test/send_rip.py
@@ -6,38 +6,36 @@ import sys
 import struct
 import time
 
-MAC_TESTER1 = '00:e0:4c:68:3c:b7'
-IP_TESTER1 = '10.0.1.2'
-IP_RIP = '10.0.0.1'
-IFACE = "enx00e04c683cb7"
+IPs = ['10.0.0.1', '10.0.1.1', '10.0.2.1', '10.0.3.1']
+IFACEs = ["enx00e04c683cb7", "enx00e04c683cb7"]
 for i in range(100, 110):
-    send(IP(dst=IP_RIP, ttl=64) /
+    port = 0
+    send(IP(dst=IP_RIPs[port], ttl=64) /
          UDP() /
          RIP(cmd=2, version=2) /
-         RIPEntry(addr='%d.140.1.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.140.2.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.140.3.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.140.4.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.140.5.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.140.6.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.140.7.0' % i, mask='255.255.255.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
-         RIPEntry(addr='%d.140.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.141.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.142.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.143.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.144.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.145.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.146.0.0' % i, mask='255.255.0.0') /
-         RIPEntry(addr='%d.147.0.0' % i, mask='255.255.0.0'),
-         iface=IFACE)
-    time.sleep(0.1)
+         RIPEntry(addr='%d.1.1.0' % i, mask='255.255.255.0') /
+         RIPEntry(addr='%d.1.2.0' % i, mask='255.255.255.0') /
+         RIPEntry(addr='%d.1.3.0' % i, mask='255.255.255.0') /
+         RIPEntry(addr='%d.1.4.0' % i, mask='255.255.255.0') /
+         RIPEntry(addr='%d.1.5.0' % i, mask='255.255.255.0') /
+         RIPEntry(addr='%d.1.6.0' % i, mask='255.255.255.0') /
+         RIPEntry(addr='%d.1.7.0' % i, mask='255.255.255.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         #     RIPEntry(addr='%d.0.0.0' % i, mask='255.0.0.0') /
+         RIPEntry(addr='%d.0.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.1.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.2.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.3.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.4.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.5.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.6.0.0' % i, mask='255.255.0.0') /
+         RIPEntry(addr='%d.7.0.0' % i, mask='255.255.0.0'))
+    time.sleep(1)
