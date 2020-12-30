85ee74f0268e8ba9f1693d62bd9eb9c6904f7f99
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-12-30 13:35:02 +0800

    linear pc01

--- test/ip_pc0.sh
+++ test/ip_pc0.sh
@@ -1,13 +1,18 @@
-PORT0=enx00e04c683cb7
-PORT1=enx00e04c680024
-PORT2=enx00e04c680f4e
-BOARD_IP_0=10.0.0.1
-BOARD_IP_1=10.0.2.1
-PC_IP_0=10.0.0.2
-PC_IP_1=10.0.2.2
-sudo ip addr add ${PC_IP_0}/24 dev $PORT0
-sudo ip route add 10.0.2.0/24 via ${BOARD_IP_0} dev $PORT0
-sudo arping -I ${PORT0} ${BOARD_IP_0} -c 2
-# ping ${PC_IP_1}
-iperf3 -c $PC_IP_1 -t 10 -R -i 2
-# sudo ping -f $PC_IP_1
+# PORT0=enx00e04c683cb7
+# PORT1=enx00e04c680024
+# PORT2=enx00e04c680f4e
+# BOARD_IP_0=10.0.0.1
+# BOARD_IP_1=10.0.2.1
+# PC_IP_0=10.0.0.2
+# PC_IP_1=10.0.2.2
+# sudo ip addr add ${PC_IP_0}/24 dev $PORT0
+# sudo ip route add 10.0.2.0/24 via ${BOARD_IP_0} dev $PORT0
+# sudo arping -I ${PORT0} ${BOARD_IP_0} -c 2
+# # ping ${PC_IP_1}
+# iperf3 -c $PC_IP_1 -t 10 -R -i 2
+# # sudo ping -f $PC_IP_1
+sudo ip addr add 10.0.0.2/24 dev enx00e04c680f4e
+sudo ip route add 10.0.0.0/8 via 10.0.0.1 dev enx00e04c680f4e
+sudo arping -I enx00e04c680f4e 10.0.0.1 -c 2
+ping -c 5 10.2.1.2
+
--- test/ip_pc1.sh
+++ test/ip_pc1.sh
@@ -1,10 +1,14 @@
 PORT0=enx00e04c683cb7
 PORT1=enx00e04c680024
 BOARD_IP_0=10.0.0.1
-BOARD_IP_1=10.0.2.1
+BOARD_IP_1=10.2.1.1
 PC_IP_0=10.0.0.2
-PC_IP_1=10.0.2.2
-sudo ip addr add ${PC_IP_1}/24 dev $PORT1
-sudo ip route add 10.0.0.0/24 via ${BOARD_IP_1} dev $PORT1
+PC_IP_1=10.2.1.2
+sudo ip addr add ${PC_IP_1}/24 dev ${PORT0}
+sudo ip route add 10.0.0.0/8 via ${BOARD_IP_1} dev ${PORT0}
 sudo arping -I ${PORT1} ${BOARD_IP_1} -c 2
 # iperf3 -s
+# sudo ip addr add 10.0.0.2/24 dev enx00e04c680f4e
+# sudo ip route add 10.0.0.0/8 via 10.0.0.1 dev enx00e04c680f4e
+# sudo arping -I enx00e04c680f4e 10.0.0.1 -c 2
+# ping -c 5 10.2.1.2
